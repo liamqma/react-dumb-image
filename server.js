83993515af1e912a4b348ee6bfaa39139565c0db
Liam Ma <liam.q.ma@gmail.com>
2016-04-21 22:51:19 +1000

    Add demo

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.dev.config');
 
-new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath
-}).listen(3000, 'localhost', function(error) {
+var app = new (require('express'))();
+var port = 3000;
+
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/demo/index.html')
+})
+
+app.listen(port, function(error) {
     if (error) {
-        console.error(error);
+        console.error(error)
     } else {
-        console.log('Demo is ready at http://localhost:3000/demo/dist/index.html');
+        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
     }
-});
+})
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,19 +1,26 @@
 var path = require("path");
+var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        app: ['./src/index']
-    },
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        './demo/index'
+    ],
     output: {
-        path: path.resolve(__dirname, "build"),
-        publicPath: "/assets/",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/static/",
         filename: "bundle.js"
     },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         loaders: [
             {
                 test: /\.js/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /node_modules/,
                 loader: 'babel',
                 query: {
                     presets: ['airbnb']

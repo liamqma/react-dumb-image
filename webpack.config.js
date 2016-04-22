a1395e9836c0eca3a67e1b60cee19e2731de336a
Liam Ma <liam.ma@mi9.com.au>
2016-04-22 10:10:55 +1000

    Add demo

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,11 @@ var path = require("path");
 var webpack = require('webpack');
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
-    entry: [
-        'webpack-hot-middleware/client',
-        './demo/index'
-    ],
+    entry: './demo/index',
     output: {
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "/static/",
+        path: path.resolve(__dirname, "demo"),
         filename: "bundle.js"
     },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
     module: {
         loaders: [
             {
